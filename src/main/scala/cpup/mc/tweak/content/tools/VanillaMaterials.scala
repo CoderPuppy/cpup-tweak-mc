f2664be4bc0234bb9a463545b0ae628fa2f6d901
CoderPuppy <cpup@cpup-laptop>
2014-09-12 21:19:48 -0400

    Make all the materials case objects

--- src/main/scala/cpup/mc/tweak/content/tools/VanillaMaterials.scala
+++ src/main/scala/cpup/mc/tweak/content/tools/VanillaMaterials.scala
@@ -6,15 +6,15 @@ import cpup.mc.tweak.CPupTweak
 object VanillaMaterials {
 	def mod = CPupTweak
 
-	object Iron extends Part.Material
+	case object Iron extends Part.Material
 	SingletonSerialization.register(Iron, s"${mod.ref.modID}:tools.vanilla.iron")
 
-	object Leather extends Part.Material
+	case object Leather extends Part.Material
 	SingletonSerialization.register(Leather, s"${mod.ref.modID}:tools.vanilla.leather")
 
-	object String extends Part.Material
+	case object String extends Part.Material
 	SingletonSerialization.register(String, s"${mod.ref.modID}:tools.vanilla.string")
 
-	object Wood extends Part.Material
+	case object Wood extends Part.Material
 	SingletonSerialization.register(Wood, s"${mod.ref.modID}:tools.vanilla.wood")
 }
